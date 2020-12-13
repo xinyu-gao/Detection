ec6121c08a8c40cd220c89c63a6f22e44ff68e1e
xinyu-gao <isgaoxy@gmai.com>
2020-12-13 13:50:27 +0800

    test

--- src/main/java/com/node/detection/controller/WebSocketController.java
+++ src/main/java/com/node/detection/controller/WebSocketController.java
@@ -1,11 +1,6 @@
 package com.node.detection.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.java_websocket.WebSocket;
-import org.java_websocket.client.WebSocketClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequestMapping("/ws")
 public class WebSocketController {
-    @Autowired
-    private WebSocketClient webSocketClient;
-
-    @PostMapping("/testWs")
-    public void test(){
-        webSocketClient.send("{\"command\":\"ask\",\"source\":\"web\",\"password\":\"\",\"value\":\"1\"}");
-    }
+//    @Autowired
+//    private WebSocketClient webSocketClient;
+//
+//    @PostMapping("/testWs")
+//    public void test(){
+//        webSocketClient.send("{\"command\":\"ask\",\"source\":\"web\",\"password\":\"\",\"value\":\"1\"}");
+//    }
 
 
 }
